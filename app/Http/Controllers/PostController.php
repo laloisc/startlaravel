8d92957baddcf57eb02041ad5ca90c32a93f2010
devlalo <lalo1isc@gmail.com>
2025-01-17 18:41:25 -0600

    validacion de campos del formulario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,6 +20,12 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        
         $post = $request->user()->posts()->create([
             'title' => $title = $request->title,
             'slug' => Str::slug($title),
@@ -34,6 +40,12 @@ class PostController extends Controller
     }
     
     public function update(Request $request, Post $post){
+
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
         $post ->update([
             'title' => $title = $request->title,
             'slug' => Str::slug($title),
