8b685f3aa374d056967369449200aa8004327508
devlalo <lalo1isc@gmail.com>
2025-04-01 08:30:26 -0600

    diseño web

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,19 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto web</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" class="h12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar">
+                </form>
+            </div>
 
-        @auth
-        <a href="{{ route('dashboard') }}" >Dashboard</a>
-        @else
-        <a href="{{ route('login') }}" >Login</a>
-        @endauth
-    </p>
-    <hr>
-    @yield('content')
+            @auth
+            <a href="{{ route('dashboard') }}" >Dashboard</a>
+            @else
+            <a href="{{ route('login') }}" >Login</a>
+            @endauth
+        </header>
+
+        @yield('content')
+    </div>
+    
 </body>
 </html>
